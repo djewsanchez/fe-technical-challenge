96b0f3499707b2da03bf19ee74ffb51983746f84
Dan <danjewellesanchez+github@gmail.com>
2025-09-25 22:34:50 +0800

    refactor: remove unnecessary import

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GrandmasterList from './components/GrandmasterList';
